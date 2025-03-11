134cb9806e8f75eecff5c839b9a686f347cbc79e
ikerquintelamira <49290823Y@iespoblenou.org>
2025-03-11 20:07:17 +0100

    main menu

--- reverbation/Scripts/menu.gd
+++ reverbation/Scripts/menu.gd
@@ -8,16 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	get_tree().change_scene_to_file("res://Escenas/world.tscn")
-
-
-func _on_start_game_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_settings_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()# Replace with function body.
+	pass
