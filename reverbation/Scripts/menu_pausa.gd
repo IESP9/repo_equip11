36828f3e0a214d1ffa8bc688a5d69fbaef7a2ffd
ikerquintelamira <49290823Y@iespoblenou.org>
2025-04-11 15:33:23 +0200

    Juego Reverbation

---
+++ reverbation/Scripts/menu_pausa.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("Pausa"):
+		toggle_pause()
+
+func toggle_pause() -> void:
+	get_tree().paused = not get_tree().paused
+	$ColorRect.visible = not $ColorRect.visible
+	$VBoxContainer/Continue.visible = not $VBoxContainer/Continue.visible
+	$VBoxContainer/Settings.visible = not $VBoxContainer/Settings.visible
+	$VBoxContainer/Exit.visible = not $VBoxContainer/Exit.visible
+	$Label.visible = not $Label.visible
+
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_continue_pressed() -> void:
+	toggle_pause()
