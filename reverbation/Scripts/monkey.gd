887479132965cc5853612382f4ebd2f9104d3481
yurii-g <31915869b@iespoblenou.org>
2025-04-24 17:00:26 +0200

    ataque mono

--- reverbation/Scripts/monkey.gd
+++ reverbation/Scripts/monkey.gd
@@ -38,8 +38,13 @@ var speed = 100
 var player_chase = false
 var player = null
 var current_direction = "none"
+var is_attacking = false
+var attack_cooldown = 1.5
+var attack_timer = 0.0
 
 func _ready():
+	$Attack/AttackArea.visible = false
+	$Attack/AttackArea.monitoring = false
 	self.y_sort_enabled = true  # Habilita YSort
 
 func _physics_process(delta: float) -> void:
@@ -70,6 +75,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = Vector2.ZERO
 		play_animation(0)  # En reposo
+		
+	if is_attacking:
+		velocity = Vector2.ZERO
+	
+
+	move_and_slide()
+
+	# Ataque cooldown
+	if attack_timer > 0:
+		attack_timer -= delta
 
 func play_animation(movement):
 	var animation = $AnimatedSprite2D
@@ -95,6 +110,49 @@ func _on_detection_area_body_exited(_body: Node2D) -> void:
 	player = null
 	player_chase = false
 
+"func _physics_process(delta):
+	if is_attacking:
+		velocity = Vector2.ZERO
+	else:
+		get_input()
+
+	move_and_slide()
+
+	# Ataque cooldown
+	if attack_timer > 0:
+		attack_timer -= delta"
+
+
+func start_attack(_body: Node2D) -> void:
+	is_attacking = true
+	attack_timer = attack_cooldown
+	$Attack/AttackArea.visible = true
+	$Attack/AttackArea.monitoring = true
+	
+	match current_direction:
+		"derecha":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("Attack_side")
+			$Attack/AttackArea.global_position = global_position + Vector2(8, 0)
+		"izquierda":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("Attack_side")
+			$Attack/AttackArea.global_position = global_position + Vector2(-100, 0)
+		"arriba":
+			$AnimatedSprite2D.play("Attack_back")
+			$Attack/AttackArea.global_position = global_position + Vector2(-45, -30)
+		"abajo":
+			$AnimatedSprite2D.play("Attack_front")
+			$Attack/AttackArea.global_position = global_position + Vector2(-45, 40)
+
+
+	await $AnimatedSprite2D.animation_finished
+	is_attacking = false
+	$Attack/AttackArea.visible = false
+	$Attack/AttackArea.monitoring = false
+func _on_attack_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and not is_attacking and attack_timer <= 0.0:
+		start_attack(body)
 "
 extends CharacterBody2D
 
