dfb49725022ad9ddc2e0043d55cb2075aa0a76ac
JoramPC <y4734970c@iespoblenou.org>
2025-05-18 17:07:12 +0200

    monos puestos

--- reverbation/Scripts/palanca_global.gd
+++ reverbation/Scripts/palanca_global.gd
@@ -8,7 +8,3 @@ var palanca_puerta5 = false
 var palanca_puerta6 = false
 var palanca_puerta7 = false
 var palanca_puerta8 = false
-var palanca_puerta9 = false
-var palanca_puerta10 = false
-var palanca_puerta11 = false
-var palanca_puerta12 = false
--- reverbation/Scripts/scripts_puertas/puerta_verdadera.gd
+++ reverbation/Scripts/scripts_puertas/puerta_verdadera.gd
@@ -1,7 +1,16 @@
 extends AnimatedSprite2D
-@onready var colision_p1: CollisionShape2D = $StaticBody2D/colision_puerta1
-@onready var puerta_1: AnimatedSprite2D = $"."
 
+@onready var colision_p1: CollisionShape2D = $StaticBody2D/colision_puerta1
+
+
+@onready var puerta_1: AnimatedSprite2D = $"."
+@onready var puerta_2: AnimatedSprite2D = $"../puerta2"
+@onready var puerta_3: AnimatedSprite2D = $"../puerta3"
+@onready var puerta_4: AnimatedSprite2D = $"../puerta4"
+@onready var puerta_5: AnimatedSprite2D = $"../puerta5"
+@onready var puerta_6: AnimatedSprite2D = $"../puerta6"
+@onready var puerta_7: AnimatedSprite2D = $"../puerta7"
+@onready var puerta_8: AnimatedSprite2D = $"../puerta8"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +21,15 @@ func _process(delta: float) -> void:
 	if palanca_global.palanca_puerta1 == true:
 		colision_p1.disabled = true
 		puerta_1.play("abierta")
+	
+	elif palanca_global.palanca_puerta2 == true:
+		colision_p1.disabled = true
+		puerta_2.play("abierta")
+	
+	
 	else:
 		puerta_1.play("horizontal")
 		colision_p1.disabled = false
+	###########################################
+	
+	
